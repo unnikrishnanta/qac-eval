eb64be0d7f28a08c43eec1bd8b34333fa9d4bdd8
Unni <unnikrishnan.t.a@gmail.com>
2019-10-23 15:06:16 +1100

    building project structure

---
+++ include/core/collecion.hpp
@@ -0,0 +1,22 @@
+#include<string>
+#include<vector>
+#include<pair>
+
+using namespace std;
+
+class Collection {
+    using sdict_t = vector<pair<string, size_t>>;
+
+    public:
+        using iterator = sdict_t::iterator;
+        using const_iterator = sdict_t::const_iterator;
+
+        void read_collection(const string& file_name);
+        const_iterator begin() const { return str_dict.begin(); }
+        const_iterator end() const { return str_dict.end(); }
+
+    private:
+        sdict_t str_dict;
+        
+};
+
---
+++ src/core/collection.cpp
@@ -0,0 +1,25 @@
+#include <core/collection.hpp>
+#include <boost/iostreams/device/mapped_file.hpp> // for mmap
+#include <iostream>
+
+using namespace std;
+using namespace boost::iostreams;
+
+void Collection::read_collection(const string& file_name){
+    mapped_file mmap(file_name, boost::iostreams::mapped_file::readonly);
+    auto file_data = mmap.const_data();
+    stringstream ss;
+    ss << file_data;
+    string line;
+
+    while (std::getline(ss, line)) {
+        if (!line.empty()){
+            istringstream iss(line);
+            string freq, targ_str;
+            getline(iss, targ_str, '\t');
+            getline(iss, freq, '\t');
+            str_dict.push_back(make_pair(targ_str, atoi(freq.c_str())));
+        }
+
+    }
+}
