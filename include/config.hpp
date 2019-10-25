667b27c4473092f67db1d0a1821e113ccacd10c0
Unni <unnikrishnan.t.a@gmail.com>
2019-10-25 15:05:36 +1100

    Reconfig gitignre

---
+++ include/config.hpp
@@ -0,0 +1,5 @@
+#include <string>
+
+using namespace std;
+
+const string WIKI_ROOT = "~/CodeBase/AutoSuggestions/PyQAC/data/wiki/";
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
+#include "core/collection.hpp"
+#include "config.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     
-    cout << "Hello world\n";
+    Collection coll_wiki;
+    coll_wiki.read_collection(WIKI_ROOT + "clickstream-agg-wiki.tsv");
     return 0;
 }
