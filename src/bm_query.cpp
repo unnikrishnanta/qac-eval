2ff3ed4462546165beef0e047ac1912724616414
Unni <unnikrishnan.t.a@gmail.com>
2020-03-25 23:05:09 +1100

    Export benchmark logs filesnames with timestamp

--- src/bm_query.cpp
+++ src/bm_query.cpp
@@ -3,53 +3,76 @@
  *
  * Written by: Unni Krishnan. On : 19/03/2020.
  */
-#include "core/pqlog.hpp"
-#include "qac_impl/htrie_wrapper.hpp"
+#include <cstdlib>
 #include <cstdint>
 #include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <sstream>
+#include "core/pqlog.hpp"
+#include "qac_impl/htrie_wrapper.hpp"
 #include <benchmark/benchmark.h>
 #include <timeprof/benchmark_query.hpp>
 
 BENCHMARK_REGISTER_F(QueryFixture, QueryHTrie)
     ->RangeMultiplier(8)
-    /* ->Ranges({{1000, 1000}, */
-    ->Ranges({{NROWS, NROWS},
+    ->Ranges({{1<<5, 1<<5},
+    /* ->Ranges({{NROWS, NROWS}, */
              {1<<4, WIKISYNTH_SIZE},
              {SYNTHLOG, SYNTHLOG}})
     ->Unit(benchmark::kMillisecond);
 
-BENCHMARK_REGISTER_F(QueryFixture, QueryMarisa)
-    ->RangeMultiplier(8)
-    /* ->Ranges({{1000, 1000}, */
-    ->Ranges({{NROWS, NROWS},
-             {1<<4, WIKISYNTH_SIZE},
-             {SYNTHLOG, SYNTHLOG}})
-    ->Unit(benchmark::kMillisecond);
+/* BENCHMARK_REGISTER_F(QueryFixture, QueryMarisa) */
+/*     ->RangeMultiplier(8) */
+/*     ->Ranges({{1<<5, 1<<5}, */
+/*     /1* ->Ranges({{NROWS, NROWS}, *1/ */
+/*              {1<<4, WIKISYNTH_SIZE}, */
+/*              {SYNTHLOG, SYNTHLOG}}) */
+/*     ->Unit(benchmark::kMillisecond); */
 
-BENCHMARK_REGISTER_F(QueryFixture, QueryDAWG)
-    ->RangeMultiplier(8)
-    /* ->Ranges({{1000, 1000}, */
-    ->Ranges({{NROWS, NROWS},
-             {1<<4, WIKISYNTH_SIZE},
-             {SYNTHLOG, SYNTHLOG}})
-    ->Unit(benchmark::kMillisecond);
+/* BENCHMARK_REGISTER_F(QueryFixture, QueryDAWG) */
+/*     ->RangeMultiplier(8) */
+/*     ->Ranges({{1<<5, 1<<5}, */
+/*     /1* ->Ranges({{NROWS, NROWS}, *1/ */
+/*              {1<<4, WIKISYNTH_SIZE}, */
+/*              {SYNTHLOG, SYNTHLOG}}) */
+/*     ->Unit(benchmark::kMillisecond); */
 
-BENCHMARK_REGISTER_F(QueryFixture, QueryDAWG)
-    ->RangeMultiplier(8)
-    /* ->Ranges({{1000, 1000}, */
-    ->Ranges({{NROWS, NROWS},
-             {1<<4, WIKISYNTH_SIZE},
-             {SYNTHLOG, SYNTHLOG}})
-    ->Unit(benchmark::kMillisecond);
+/* BENCHMARK_REGISTER_F(QueryFixture, QueryDAWG) */
+/*     ->RangeMultiplier(8) */
+/*     ->Ranges({{1<<5, 1<<5}, */
+/*     /1* ->Ranges({{NROWS, NROWS}, *1/ */
+/*              {1<<4, WIKISYNTH_SIZE}, */
+/*              {SYNTHLOG, SYNTHLOG}}) */
+/*     ->Unit(benchmark::kMillisecond); */
 
 BENCHMARK_REGISTER_F(QueryFixture, QueryIncNgTrie)
     ->RangeMultiplier(8)
-    /* ->Ranges({{1000, 1000}, */
-    ->Ranges({{NROWS, NROWS},
+    ->Ranges({{1<<5, 1<<5},
+    /* ->Ranges({{NROWS, NROWS}, */
              {1<<4, WIKISYNTH_SIZE},
              {SYNTHLOG, SYNTHLOG}})
     ->Unit(benchmark::kMillisecond);
 
+string get_current_datetime() {
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%d-%m-%Y_%H-%M-%S");
+    auto str = oss.str();
+    return str;
+}
+
+/* Set BENCHMARK_OUT_FORMAT and BENCHMARK_OUT environment variables to redirect
+ * the output to the format and file. 
+ * Output filename: ../export/data/current_datatime.csv
+ */
+void setenv_vars(const string& format="csv") {
+    std::stringstream ss_out;
+    ss_out << "../export/data/" << get_current_datetime() << "-query.csv";
+    setenv("BENCHMARK_OUT_FORMAT", format.c_str(), 1);
+    setenv("BENCHMARK_OUT", ss_out.str().c_str(), 1);
+}
 
 /* Declaring static members */
 Collection QueryBase::coll;
