1acf1602f286cf08bda601d7457cbf15975e9f57
Unni <unnikrishnan.t.a@gmail.com>
2020-04-08 20:31:01 +1000

    Read from string score file instead of just string

--- Python/convert_binglog.py
+++ Python/convert_binglog.py
@@ -17,6 +17,8 @@ from unidecode import unidecode
 import sys
 import csv
 from collections import Counter
+from collections import defaultdict
+import re
 
 maxInt = sys.maxsize
 while True:
@@ -30,32 +32,50 @@ while True:
         maxInt = int(maxInt/10)
 
 
-def convert_to_ascii(str_list, verbose=False):
-    """ Conver the strings in str_list from utf-8 to ASCII.
+def convert_to_ascii(str_dict, verbose=False):
+    """ Conver the strings in str_dict from utf-8 to ASCII.
     """
     ascii_list = list()
     non_converting = 0
     if verbose:
         print('\nNon converting strings:')
-    for line in str_list:
-        ascii_str = unidecode(line)
+    for line in str_dict:
+        ascii_str = unidecode(line[0])
         if(len(ascii_str)):
-            ascii_list.append(ascii_str.strip())
+            ascii_list.append((ascii_str.strip(), line[1]))
         else:
             non_converting += 1
     print(non_converting, " conversations did not convert to ASCII")
     return ascii_list
 
+def preprocess_str(s): 
+    s = s.strip()
+    s = s.lower()
+    s = s.replace(u"–", "_")  # Replace all hyphens by whitespaces
+    if not hasattr(preprocess_str, "pattern"): # Initialize it
+        preprocess_str.pattern = re.compile('[\W_]+', re.UNICODE)
+    filtered = preprocess_str.pattern.sub('_', s).strip("_")
+    return filtered
+
 def read_file(file_name):
-    str_list = list()
+    str_dict = list()
     empty_lines = 0
     with open(file_name, "r", encoding='utf-8') as infile:
         for line in infile.readlines():
             line = line.strip()
             if len(line) == 0: # strange!
                 empty_lines += 1
-            str_list.append(line.lower())  # line -> partial queries
-    return str_list
+            line = line.split('\t')
+            print(line)
+            s = preprocess_str(line[0])
+            str_dict.append((s, int(line[1])))  # line -> partial queries
+    return str_dict
+
+def freq_accum(str_dict):
+    freq_map = defaultdict(int)
+    for line in str_dict: 
+        freq_map[line[0]] += line[1]
+    return freq_map
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert utf-8 log to ASCII')
@@ -71,7 +91,7 @@ if __name__ == "__main__":
         print('Running in verbose')
         verbose = True
 
-    str_list = read_file(args['infile'])
+    str_dict = read_file(args['infile'])
     print("File ", args['infile'], " loaded.")
 
     if args['outfile'] :
@@ -79,8 +99,8 @@ if __name__ == "__main__":
     else:
         export_file = "ascii-encoded.tsv"
 
-    ascii_data = convert_to_ascii(str_list, verbose=verbose)
-    str_freq = Counter(ascii_data)
+    ascii_data = convert_to_ascii(str_dict, verbose=verbose)
+    str_freq = freq_accum(ascii_data)
     # https://stackoverflow.com/a/3191811/937153
     with open(export_file, "w", encoding='ascii', newline='') as outfile:
         for k in str_freq:
