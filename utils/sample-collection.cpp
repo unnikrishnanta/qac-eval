a8f897fc51ad8411c61345a8b8adb0e133643633
Unni <unnikrishnan.t.a@gmail.com>
2020-04-12 03:28:40 +1000

    Cweb sampler

--- utils/sample-collection.cpp
+++ utils/sample-collection.cpp
@@ -1,6 +1,7 @@
 /* Randomly shuffle the collection to the given size. To obtain consistent
  * shuffling across multiple runs, the sampled indexes are stored in a file. 
  * The indexes can be loaded from this file to perform the sampling later. 
+    g++ -std=c++17 sample-collection.cpp -o sample-collection
  */
 #include <iostream>
 #include <algorithm>
@@ -11,7 +12,8 @@
 #include <cassert>
 #include <numeric>
 #include <random>
-#include "../include/myprog_opts.hpp"
+#include "myprog_opts.hpp"
+#include <sstream>
 #include <fstream>
 
 using namespace std;
@@ -20,6 +22,7 @@ using namespace std;
  * sub vector of size sample_size
  */
 vector<size_t> sample_indexes(const size_t& sample_size, const size_t& max_size){
+  assert(sample_size <= max_size);
   std::srand (unsigned ( std::time(0) ) );
   std::vector<size_t> index_vec(max_size);
   // Fill with 0, 1, ..., max_size-1
@@ -32,7 +35,7 @@ vector<size_t> sample_indexes(const size_t& sample_size, const size_t& max_size)
 }
 
 template <typename T>
-void index_to_file(const vector<T>& data, const string file_name){
+void vec_to_file(const vector<T>& data, const string file_name){
     ofstream myfile;
     myfile.open (file_name);
     for (const auto& i : data) {
@@ -41,30 +44,59 @@ void index_to_file(const vector<T>& data, const string file_name){
     myfile.close();
 }
 
-vector<string> sample_collecton(const vector<string>& coll, const vector<size_t>& idx){
+template<typename T>
+T getline_as(const string& s)
+{
+    std::stringstream convert(s);
+    T value;
+    convert >> value;
+    return value;
+}
+
+vector<string> sample_collecton(const vector<string>& coll,
+        const vector<size_t>& idx){
     assert(idx.size() <= coll.size());
     vector<string> sampled; 
     for (const auto& i: idx) {
+        cout << "i = " << i << "\n";
+       assert(i < coll.size());
        sampled.push_back(coll[i]);
     }
     return sampled;
 }
 
-
 template <typename T>
-vector<T> load_file(const string& file_name) {
+void load_file(vector<T>& ret_vec, const string& file_name,
+        const string type="str") {
     vector<T> data;
     ifstream infile(file_name);
-    if (inputFile) {  // test file open   
-        T value;
-        while ( infile >> value ) {
-            data.push_back(value);
+    if (infile) {  // test file open   
+        std::string line;
+        while (std::getline(infile, line)) {
+            data.push_back(getline_as<T>(line));
         }
     }
-    else 
+    else {
+        std::cerr << "To generate index file (if missing), run with -e.\n";
         throw std::runtime_error("Error opening " + file_name );
-    
-    return data;
+    }
+    ret_vec.swap(data);
+}
+
+void load_collection(vector<string>& ret_vec, const string& file_name) {
+    vector<string> data;
+    ifstream infile(file_name);
+    if (infile) {  // test file open   
+        std::string line;
+        while (std::getline(infile, line)) {
+            data.push_back(line);
+        }
+    }
+    else {
+        std::cerr << "To generate index file (if missing), run with -e.\n";
+        throw std::runtime_error("Error opening " + file_name );
+    }
+    ret_vec.swap(data);
 }
 
 int main(int argc, char *argv[])
@@ -72,10 +104,10 @@ int main(int argc, char *argv[])
     if (cmdOptionExists(argv, argv + argc, "-h")) {
         std::cout << "Program options\n";
         std::cout << "==========================================================\n";
-        std::cout << "-h Print usage.";
+        std::cout << "-h Print usage.\n";
         std::cout << "-e Export sampled indexes\n";
         std:: cout << "-l file_name. Load sampled indexes from file\n";
-        std::cout << "-s Sample size.\n";
+        std::cout << "-s Sample size. Default: 128M\n";
         std::cout << "-i Input file_name\n";
         std::cout << "-o Output file_name\n";
         return 0;
@@ -87,7 +119,7 @@ int main(int argc, char *argv[])
     string index_file = "cweb-index.txt";
     char *idxf = getCmdOption(argv, argv + argc, "-l");
     if(idxf){
-        index_file = std::string(idxg);
+        index_file = std::string(idxf);
         std::cout  << "Loading indexes from: " << index_file << "\n";
     }
     string infile;
@@ -112,19 +144,25 @@ int main(int argc, char *argv[])
     std::cout  << "Sample size: " << sample_size << "\n";
 
     std::cout << "Reading collection\n";
-    vector<string> coll = load_file(infile);
+    vector<string> coll;
+    load_collection(coll, infile);
     auto coll_size = coll.size();
     std::cout << "Collection size " << coll_size << "\n";
+    
     if(sample) {
         auto idx_sample = sample_indexes(sample_size, coll_size);
-        index_to_file("cweb-index.txt");
+        vec_to_file(idx_sample, "cweb-index.txt");
         return 0;
     }
 
-    vector<size_t> idx = load_file(index_file);
+    vector<size_t> idx; 
+    load_file(idx, index_file);
+    cout << "Index file: \n";
+    for (const auto& i : idx) {
+       cout << i << "\n"; 
+    }
     auto sampled = sample_collecton(coll, idx);
-    
-
+    vec_to_file(sampled, outfile);
 
     return 0;
 }
