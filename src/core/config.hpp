26060d40365d69b9df62ddd135b7c77a6fd3e949
Unni <unnikrishnan.t.a@gmail.com>
2020-04-19 01:57:10 +1000

    Added file paths for Bodo's server

--- src/core/config.hpp
+++ src/core/config.hpp
@@ -14,34 +14,57 @@
 
 #define WIKI_NCONV 1206653
 #define CWEB_NCONV 1264108
-#define BING_NCONV 10  // TODO: Enter number of conversations. 
+#define BING_NCONV 1968265  
 
 #define WIKI_NROWS 4560699
-#define CWEB_NROWS 125000000  // TODO: Check if the file has been sampled
-#define BING_NROWS 10   // TODO: Enter # lines in the processed Bing collection
-
-#if COLLECTION==WIKI 
-#define NROWS WIKI_NROWS
-#define SYNTHLOG "../../synth_log/data/wiki-synthlog.tsv"
-#define PQLOG_NCONV WIKI_NCONV
-#elif COLLECTION==CWEB 
-#define NROWS CWEB_NROWS
-#define SYNTHLOG "../../synth_log/data/cweb-synthlog.tsv"
-#define PQLOG_NCONV CWEB_NCONV
-#else
-#define NROWS BING_NROWS
-#define SYNTHLOG ""  // TODO: Bing log (ascii) file name. 
-#define PQLOG_NCONV BING_NCONV
-#endif
+#define CWEB_NROWS 125000000  
+#define BING_NROWS 11578960   
 
 #define NCOMP 8  // Default number of completions 
+#define MAX_STRLEN 32  // Max length of strings indexed
 
-const std::string WIKI_ROOT = "../../AutoSuggestions/PyQAC/data/wiki/";
-const std::string CWEB_ROOT = "../../AutoSuggestions/PyQAC/data/"
-                            "Clue-500M-Anchor-Log-External/";
-const std::string BING_ROOT = "";  // TODO : Dir path containing Bing collection
-const std::string WIKI_COLLECTION = "clickstream-agg-wiki-32B.tsv";
-const std::string CWEB_COLLECTION = "clueweb-agg-32B-sample.tsv";
-const std::string BING_COLLECTION = "";  // TODO: Bing collection file name
+#if defined(unix) || defined(__unix__) || defined(__unix)
+    #if collection==wiki 
+    #define NROWS WIKI_NROWS
+    #define SYNTHLOG "../../synth_log/data/wiki-synthlog.tsv"
+    #define PQLOG_NCONV WIKI_NCONV
+    #elif COLLECTION==CWEB 
+    #define NROWS CWEB_NROWS
+    #define SYNTHLOG "../../synth_log/data/cweb-synthlog.tsv"
+    #define PQLOG_NCONV CWEB_NCONV
+    #else
+    #define NROWS BING_NROWS
+    #define SYNTHLOG "../../final_bing_log_ascii.tsv" 
+    #define PQLOG_NCONV BING_NCONV
+    #endif
+    const std::string WIKI_ROOT = "../../AutoSuggestions/PyQAC/data/wiki/";
+    const std::string CWEB_ROOT = "../../AutoSuggestions/PyQAC/data/Clue-500M-Anchor-Log-External/";
+    const std::string BING_ROOT = "";  
+    const std::string WIKI_COLLECTION = "clickstream-agg-wiki-32B.tsv";
+    const std::string CWEB_COLLECTION = "clueweb-agg-32B-sample.tsv";
+    const std::string BING_COLLECTION = "";
+#endif  // ifdef unix
+
+#if defined(_WIN32)
+    #if COLLECTION==WIKI 
+    #define NROWS WIKI_NROWS
+    #define SYNTHLOG "../../synth_log/data/wiki-synthlog.tsv"
+    #define PQLOG_NCONV WIKI_NCONV
+    #elif COLLECTION==CWEB 
+    #define NROWS CWEB_NROWS
+    #define SYNTHLOG "../../synth_log/data/cweb-synthlog.tsv"
+    #define PQLOG_NCONV CWEB_NCONV
+    #else
+    #define NROWS BING_NROWS
+    #define SYNTHLOG "../../final_bing_log_ascii.tsv"
+    #define PQLOG_NCONV BING_NCONV
+    #endif
+    const std::string WIKI_ROOT = "../../AutoSuggestions/PyQAC/data/wiki/";
+    const std::string CWEB_ROOT = "../../AutoSuggestions/PyQAC/data/Clue-500M-Anchor-Log-External/";
+    const std::string BING_ROOT = "../../";
+    const std::string WIKI_COLLECTION = "clickstream-agg-wiki-32B.tsv";
+    const std::string CWEB_COLLECTION = "clueweb-agg-32B-sample.tsv";
+    const std::string BING_COLLECTION = "completionFrequencies_processed.tsv";
+#endif  // if defined (_WIN32)
 
 #endif /* ifndef CONFIG_H */
