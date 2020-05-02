7aea0c4723854955d0092f645954445ad2aaeda5
Unni <unnikrishnan.t.a@gmail.com>
2020-05-02 21:16:03 +1000

    Updated for new heap counters

--- src/mem_benchmark.cpp
+++ src/mem_benchmark.cpp
@@ -1,29 +1,36 @@
 #include <cstdint>
 #include<iostream>
 #include "core/collection.hpp"
+#include "core/config.hpp"
 #include "core/pqlog.hpp"
 #include <qac_impl.hpp>
 #include <thread>
 #include <chrono>
 #include <memprof/mprof_core.hpp>
 #include "core/heap_mem.h"
+#include "qac_impl/marisa_wrapper.hpp"
 
 /* using namespace std; */
 /* using namespace CGE; */
 
+
 int main(int argc, char *argv[])
 {
+    std::cerr << "Platform detected: " << PLATFORM << "\n";
     //todo: take collection as an argument
-    size_t n_rows = SIZE_MAX;
+    size_t n_rows = 10;
+    Collection coll; 
+    coll.read_collection(COLLECTION, n_rows, true);
 
-    Collection coll_wiki;
-    cout << "Reading collection\n";
-    coll_wiki.read_collection('w', n_rows, true);
-    HTrieCompleter ht_comp;
-    ht_comp.build_index(coll_wiki);
-    /* MemProfiler<HTrieCompleter> htrie_mprof(COLLECTION); */
-    /* htrie_mprof.set_nrows(n_rows); */
-    /* htrie_mprof.mem_bm_build(); */
+    {
+        MemProfiler<HTrieCompleter> htrie_mprof;
+        htrie_mprof.mem_bm_build(coll);
+    }
+
+    {
+        MemProfiler<MarisaCompleter> mtrie_mprof;
+        mtrie_mprof.mem_bm_build(coll);
+    }
 
     /* MemProfiler<MarisaCompleter> mtrie_mprof(COLLECTION); */
     /* mtrie_mprof.set_nrows(n_rows); */
